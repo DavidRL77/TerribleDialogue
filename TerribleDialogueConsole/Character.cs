4f344ace6afbac9ef5d4cd7c9e97b1886c90d895
Davicro <david.rollonla@gmail.com>
2026-01-11 12:29:39 +0100

    Added characters

---
+++ TerribleDialogueConsole/Character.cs
@@ -0,0 +1,16 @@
+﻿using Davicro.TerribleDialogue;
+
+namespace TerribleDialogueConsole
+{
+    internal record Character
+    {
+        public string Name { get; }
+        public DialogueProcessor Processor { get; }
+
+        public Character(string name, DialogueProcessor processor)
+        {
+            Name = name;
+            Processor = processor;
+        }
+    }
+}
--- TerribleDialogueConsole/Program.cs
+++ TerribleDialogueConsole/Program.cs
@@ -1,32 +1,68 @@
 ﻿using Davicro.TerribleDialogue;
 using Sprache;
+using System.Text;
 
 namespace TerribleDialogueConsole
 {
     internal class Program
     {
+        private static readonly Random random = new Random();
+
+        private static readonly Character[] characters = { 
+            CreateCharacter("John", "Dialogue/john.tdlg"),
+            CreateCharacter("Byte", "Dialogue/byte.tdlg"),
+            CreateCharacter("Cute anime girl", "Dialogue/cute.tdlg")
+        };
+
         static void Main(string[] args)
         {
-            string dialogueFile = "test.tdlg";
-            Random rnd = new Random();
+            Console.OutputEncoding = Encoding.UTF8;
 
-            DialogueObject obj = DialogueGrammar.Dialogue.Parse(File.ReadAllText(Path.Join("Dialogue", dialogueFile)));
-            DialogueProcessor processor = new DialogueProcessor(obj, rnd.Next);
-
-            while(!processor.HasEndedDialogue)
+            while(true)
             {
-                if(processor.HasNextLine())
+                Console.WriteLine("Who do you want to talk to?");
+                for(int i = 0; i < characters.Length; i++)
                 {
-                    Console.Write(processor.GetNextLine().Text);
-                    Console.ReadLine();
-                } 
-                else
+                    Console.WriteLine(characters[i].Name);
+                }
+                Console.Write("> ");
+
+                string answer = Console.ReadLine();
+                Character character = characters.FirstOrDefault(c => c.Name.ToLower() == answer.ToLower().Trim());
+                if(character == null)
                 {
-                    processor.EndNode();
-                    Console.WriteLine(new String('=', Console.BufferWidth));
+                    Console.WriteLine("No character by that name");
+                    Console.WriteLine();
+                    continue;
                 }
 
+                Console.Clear();
+                TalkToCharacter(character);
+                Console.Clear();
             }
+        }
+
+        private static void TalkToCharacter(Character c)
+        {
+            if(c.Processor.HasEndedDialogue)
+            {
+                Console.Write($"{c.Name} has nothing to say.");
+                Console.ReadLine();
+                return;
+            }
+
+            while(c.Processor.HasNextLine()) 
+            {
+                Console.Write(c.Processor.GetNextLine().Text);
+                Console.ReadLine();
+            }
+
+            c.Processor.EndNode();
+        }
+
+        private static Character CreateCharacter(string name, string dialogueFile)
+        {
+            return new Character(name, new DialogueProcessor(DialogueGrammar.Dialogue.Parse(File.ReadAllText(dialogueFile)), random.Next));
         }
 
     }

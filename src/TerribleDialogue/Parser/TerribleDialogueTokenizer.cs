87051c0185ae485c01e2931851b5eff40c9153cd
davicro <david.rollonla@gmail.com>
2026-05-06 13:04:05 +0200

    Added tokenizer

--- src/TerribleDialogue/Parser/TerribleDialogueTokenizer.cs
+++ src/TerribleDialogue/Parser/TerribleDialogueTokenizer.cs
@@ -1,11 +1,18 @@
 using Superpower;
 using Superpower.Model;
 using Superpower.Parsers;
+using Superpower.Tokenizers;
 
 namespace TerribleDialogue.Parser
 {
     internal static class TerribleDialogueTokenizer
     {
+        /// <summary>
+        /// Matches any character in between quotes (") and ignores any character preceded by a backslash (\).
+        /// <para>ONLY matches, does not return anything.</para>
+        /// </summary>
+        /// The reason why we use a Unit parser instead of returning the content, is that we want this parser to be as permissive as possible,
+        /// and don't want to actually allocate any string result until later on in the parsing process.
         internal static readonly TextParser<Unit> QuotedTextToken =
             from open in Character.EqualTo('"')
             from content in Character.EqualTo('\\') .IgnoreThen(Character.AnyChar).Value(Unit.Value).Try()
@@ -13,5 +20,17 @@ namespace TerribleDialogue.Parser
             .IgnoreMany()
             from close in Character.EqualTo('"')
             select Unit.Value;
+
+        public static readonly Tokenizer<TerribleDialogueToken> Tokenizer =
+            new TokenizerBuilder<TerribleDialogueToken>()
+            .Ignore(Span.WhiteSpace)
+            .Match(Character.EqualTo('['), TerribleDialogueToken.LSquareBracket)
+            .Match(Character.EqualTo(']'), TerribleDialogueToken.RSquareBracket)
+            .Match(Character.EqualTo(':'), TerribleDialogueToken.Colon)
+            .Match(Character.EqualTo(','), TerribleDialogueToken.Comma)
+            .Match(Character.EqualTo('='), TerribleDialogueToken.Equals)
+            .Match(QuotedTextToken, TerribleDialogueToken.QuotedText)
+            .Match(Identifier.CStyle, TerribleDialogueToken.Identifier)
+            .Build();
     }
 }
--- test/TerribleDialogue.Tests/ParserTests.cs
+++ test/TerribleDialogue.Tests/ParserTests.cs
@@ -1,20 +1,31 @@
 using Superpower;
+using Superpower.Parsers;
 using TerribleDialogue.Parser;
 
 namespace TerribleDialogue.Tests
 {
     internal class ParserTests
     {
-        [TestCase("\"Hello this is a \"quoted\" \n text!\"", true)]
+        [TestCase(@"""Hello this is a \""quoted\"" \n text!""", true)]
         [TestCase("This text has no quotes!", false)]
         [TestCase("\"Oops, forgot to close the quotes!", false)]
         public void QuotedTextTokenTest(string input, bool shouldParse)
         {
             var result = TerribleDialogueTokenizer.QuotedTextToken.TryParse(input);
-            if(!result.HasValue)
-                TestContext.Out.WriteLine($"Error message: {result}");
+            TestContext.Out.WriteLine($"Result: {result}");
 
-            Assert.That(shouldParse, Is.EqualTo(result.HasValue));
+            Assert.That(result.HasValue, Is.EqualTo(shouldParse));
+        }
+
+        [TestCase(@"""quoted text"" : , [""more quoted text""]", true)]
+        [TestCase(@"""incomplete text", false)]
+        [TestCase(@"""what is this"" ?", false)]
+        public void TokenizerTest(string input, bool shouldTokenize)
+        {
+            var result = TerribleDialogueTokenizer.Tokenizer.TryTokenize(input);
+            TestContext.Out.WriteLine($"Result: {result}");
+
+            Assert.That(result.HasValue, Is.EqualTo(shouldTokenize));
         }
     }
 }

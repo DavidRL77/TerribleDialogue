1e48d7ce4eb2edbc8d0e094b6218eda15c70b91f
Davicro <david.rollonla@gmail.com>
2026-05-08 17:03:50 +0200

    Added text parsers

--- test/TerribleDialogue.Tests/ParserTests.cs
+++ test/TerribleDialogue.Tests/ParserTests.cs
@@ -17,7 +17,7 @@ namespace TerribleDialogue.Tests
             Assert.That(result.HasValue, Is.EqualTo(shouldParse));
         }
 
-        [TestCase(@"""quoted text"" : , identifier +12.5 -2 [""more quoted text""]", true)]
+        [TestCase(@"""quoted text"" : , identifier +12.5 -2 [value=""more quoted text""]", true)]
         [TestCase(@"""incomplete text", false)]
         [TestCase(@"""what is this"" ?", false)]
         [TestCase(@"delimiters are 2imporant", false)]
@@ -37,6 +37,24 @@ namespace TerribleDialogue.Tests
             TestContext.Out.WriteLine($"Result: {result}");
 
             Assert.That(result.HasValue, Is.True);
+        }
+
+        [TestCase(@"""This is some text!!!""", "This is some text!!!")]
+        [TestCase(@"""Some nasty \r \""escape\"" \\ sequences!!! \n """, "Some nasty \r \"escape\" \\ sequences!!! \n ")]
+        public void StringParserTest(string input, string expected) 
+        {
+            string result = TerribleDialogueTextParsers.String.Parse(input);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [TestCase("12", 12)]
+        [TestCase("-12", -12)]
+        [TestCase("1.10", 1.1f)]
+        [TestCase("-1.10", -1.1f)]
+        public void NumericParserTest(string input, float expected)
+        {
+            float result = TerribleDialogueTextParsers.Number.Parse(input);
+            Assert.That(expected, Is.EqualTo(result));
         }
     }
 }
